463b1756b77b92c61e050ba163e90aa2bab600a4
letsinnovate-code <ap1106561@gmail.com>
2025-04-28 00:57:31 +0530

    created authRoutes and authControllers

---
+++ backend/src/controllers/auth.controllers.js
@@ -0,0 +1,44 @@
+
+import bcrypt from "bcryptjs"
+import db from "../libs/db.js"
+import { UserRole } from "../generated/prisma/index.js";
+
+
+export const register = async (req,res)=>{
+
+    const {email , password,name} = req.body;
+
+    try {
+        const existingUser = await db.user.findUnique({
+            where:{
+                email
+            }
+        })
+
+        if(existingUser){
+            return res.status(400).json({
+                error:"User already exists"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password , 10)
+
+        const newUser = await db.user.create({
+            date:{
+                email,
+                password:hashedPassword,
+                name,
+                role:UserRole.USER
+            }
+        })
+
+    } catch (error) {
+        
+    }
+
+}
+
+export const login = async (req,res)=>{}
+export const logout = async (req,res)=>{}
+export const check = async (req,res)=>{}
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 const app = express();
+
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.send("hello guys wellcome to the leetlab🔥")
+}) 
+
+app.use("/api/v1/auth", authRoutes)
 
 app.listen(8080,()=>{
     console.log("Server is running on port 8080")
